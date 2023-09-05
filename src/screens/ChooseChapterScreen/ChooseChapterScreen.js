2920e4626a24352025c83f1986c1c792fd4b6362
comboy3004 <brandonqueen87@gmail.com>
2023-09-05 17:04:33 -0400

    ChooseChapterScreen finished open/close books.

--- src/screens/ChooseChapterScreen/ChooseChapterScreen.js
+++ src/screens/ChooseChapterScreen/ChooseChapterScreen.js
@@ -3,8 +3,10 @@ import { Text, StyleSheet, View, FlatList, Button } from "react-native";
 import { List } from "react-native-paper";
 import { bibleData } from "../../constants/bibleData";
 
-const MyComponent = () => {
+const ChooseChapterScreen = () => {
 	const [list, setList] = useState(bibleData);
+	const [isTestamentChosen, setIsTestamentChosen] = useState(false);
+	const [isBookChosen, setIsBookChosen] = useState(false);
 
 	const handleTestamentPress = (item) => {
 		if (item.testament === "Old Testament") {
@@ -12,6 +14,7 @@ const MyComponent = () => {
 				const newState = [...prevState];
 				newState[0].isExpanded = !newState[0].isExpanded;
 				newState[1].isExpanded = false;
+				setIsTestamentChosen(!isTestamentChosen);
 				return newState;
 			});
 		} else {
@@ -19,19 +22,39 @@ const MyComponent = () => {
 				const newState = [...prevState];
 				newState[1].isExpanded = !newState[1].isExpanded;
 				newState[0].isExpanded = false;
+				setIsTestamentChosen(!isTestamentChosen);
 				return newState;
 			});
 		}
 	};
 
-	const handleBookPress = (book, index, item) => {
-		testamentClicked = item.testament;
-		const testamentIndex = testamentClicked === "Old Testament" ? 0 : 1;
+	const handleBookPress = (bookIndex, item, index) => {
+		let tempList = list.map((testament) => ({
+			...testament,
+			books: testament.books.map((book) => ({
+				...book,
+				isExpanded: false,
+			})),
+		}));
+		tempList[index].books[bookIndex].isExpanded =
+			!item.books[bookIndex].isExpanded;
+		setList(tempList);
+		setIsBookChosen(!isBookChosen);
+	};
+
+	const Header = () => {
+		if (isBookChosen) {
+			return <Text style={styles.header}>Choose Chapter</Text>;
+		} else if (isTestamentChosen) {
+			return <Text style={styles.header}>Choose Book</Text>;
+		} else {
+			return <Text style={styles.header}>Choose Testament</Text>;
+		}
 	};
 
 	return (
 		<View style={styles.root}>
-			<Text style={styles.header}>Choose Testament</Text>
+			<Header />
 			<FlatList
 				data={list}
 				keyExtractor={(item, index) => index.toString()}
@@ -41,13 +64,15 @@ const MyComponent = () => {
 						title={item.testament}
 						expanded={item.isExpanded}
 						onPress={() => handleTestamentPress(item)}>
-						{item.books.map((book, index) => (
+						{item.books.map((book, bookIndex) => (
 							<List.Accordion
 								//books
 								title={book.title}
 								expanded={book.isExpanded}
-								onPress={() => handleBookPress(book, index, item)}>
-								<Text style={{ color: "white" }}>Number of Chapters</Text>
+								onPress={() => handleBookPress(bookIndex, item, index)}>
+								<Text style={styles.chapters}>
+									This book has {item.books[bookIndex].numOfChapters} chapters
+								</Text>
 							</List.Accordion>
 						))}
 					</List.Accordion>
@@ -65,9 +90,15 @@ const styles = StyleSheet.create({
 		color: "#f5f5f5",
 		fontWeight: "800",
 		fontSize: 24,
-		padding: 16,
+		padding: 24,
 		textAlign: "center",
+	},
+	chapters: {
+		color: "#f5f5f5",
+		fontSize: 16,
+		fontWeight: "800",
+		padding: 16,
 	},
 });
 
-export default MyComponent;
+export default ChooseChapterScreen;
