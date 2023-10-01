8f97b81763379cb0b39d19388d58565406dc8c85
comboy3004 <brandonqueen87@gmail.com>
2023-10-01 14:36:19 -0400

    Added contact section to Settings screen.

--- src/screens/SettingsScreen/SettingsScreen.js
+++ src/screens/SettingsScreen/SettingsScreen.js
@@ -1,68 +1,90 @@
-import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	View,
+	ScrollView,
+	Pressable,
+	Linking,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const SettingsScreen = () => {
 	const navigation = useNavigation();
+	const handleReadAgainPress = () => {
+		alert("read again pressed");
+	};
+	const handleResetAllPress = () => {
+		alert("reset all pressed");
+	};
+
+	const handleContactPress = () => {
+		Linking.openURL("mailto:levelupbible@gmail.com");
+	};
 
 	return (
 		<View style={styles.root}>
-			<View style={styles.card}>
-				<ScrollView style={{ width: "100%", flex: 0 }}>
-					<Text style={styles.header}>Settings</Text>
-					<View
-						style={{
-							width: "100%",
-							alignItems: "flex-start",
-							justifyContent: "flex-start",
-						}}>
-						<View style={styles.optionContainer}>
-							<Text style={[styles.optionTitle, { color: "#7b6efa" }]}>
-								Read Again
+			<ScrollView
+				style={{ width: "100%", flex: 0 }}
+				showsVerticalScrollIndicator={false}>
+				<Text style={styles.header}>Settings</Text>
+				<View
+					style={{
+						width: "100%",
+						alignItems: "flex-start",
+						justifyContent: "flex-start",
+					}}>
+					<View style={styles.optionContainer}>
+						<Text style={[styles.optionTitle]}>Read Again</Text>
+						<Text style={styles.textDescription}>
+							Reset chapter, book and rewards progress but{" "}
+							<Text
+								style={{
+									fontWeight: "800",
+									fontSize: 18,
+									color: "#7b6efa",
+								}}>
+								preserve your current overall points
 							</Text>
-							<Text style={styles.textDescription}>
-								Reset your chapter and book progress as well as reset your
-								rewards but{" "}
-								<Text
-									style={{
-										fontWeight: "800",
-										fontSize: 18,
-										color: "#7b6efa",
-									}}>
-									preserve your current overall points
-								</Text>
-								. (This option is ideal for multiple re-readings of the Bible.)
-							</Text>
-							<Pressable
-								style={[styles.button, { backgroundColor: "#695DDA" }]}>
-								<Text style={styles.buttonText}>Read Bible Again</Text>
-							</Pressable>
-						</View>
-						<View style={styles.optionContainer}>
-							<Text style={[styles.optionTitle, { color: "#db3537" }]}>
-								Reset All Data
-							</Text>
-							<Text style={styles.textDescription}>
-								Reset all data. Same as option above but{" "}
-								<Text
-									style={{
-										fontWeight: "800",
-										fontSize: 18,
-										color: "#db3537",
-									}}>
-									this will also reset your current points back to zero.
-								</Text>
-							</Text>
-							<Pressable
-								style={[styles.button, { backgroundColor: "#db3537" }]}>
-								<Text style={styles.buttonText}>Reset All Data</Text>
-							</Pressable>
-						</View>
-						<View style={styles.optionContainer}>
-							<Text style={styles.optionTitle}>Contact</Text>
-						</View>
+							. (This option is ideal for continuing to gain points with
+							multiple re-readings of the Bible.)
+						</Text>
+						<Pressable
+							style={[styles.button, { backgroundColor: "#695DDA" }]}
+							onPress={handleReadAgainPress}>
+							<Text style={styles.buttonText}>Read Bible Again</Text>
+						</Pressable>
 					</View>
-				</ScrollView>
-			</View>
+					<View style={styles.optionContainer}>
+						<Text style={[styles.optionTitle]}>Reset All Data</Text>
+						<Text style={styles.textDescription}>
+							Reset all data. Same as option above but{" "}
+							<Text
+								style={{
+									fontWeight: "800",
+									fontSize: 18,
+									color: "#db3537",
+								}}>
+								this will also reset your current points back to zero.
+							</Text>
+						</Text>
+						<Pressable
+							style={[styles.button, { backgroundColor: "#db3537" }]}
+							onPress={handleResetAllPress}>
+							<Text style={styles.buttonText}>Reset All Data</Text>
+						</Pressable>
+					</View>
+					<View style={[styles.optionContainer, { marginBottom: 30 }]}>
+						<Text style={styles.optionTitle}>Contact</Text>
+						<Text style={styles.textDescription}>
+							If you would like to get in touch to report an issue, ask a
+							question, or just to say hello, please click the button below.
+						</Text>
+						<Pressable style={styles.button} onPress={handleContactPress}>
+							<Text style={styles.buttonText}>Contact Us</Text>
+						</Pressable>
+					</View>
+				</View>
+			</ScrollView>
 		</View>
 	);
 };
@@ -74,22 +96,14 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center",
-	},
-	card: {
-		width: "95%",
-		height: "95%",
-		padding: 18,
-		backgroundColor: "rgb(11,14,29)",
-		borderRadius: 12,
-		justifyContent: "center",
-		alignItems: "center",
+		padding: 10,
 	},
 	header: {
-		color: "#f5f5f5",
-		fontWeight: "800",
 		fontSize: 30,
+		fontWeight: "900",
 		textAlign: "center",
-		paddingBottom: 16,
+		color: "#f5f5f5",
+		margin: 24,
 	},
 	optionContainer: {
 		paddingVertical: 12,
@@ -101,7 +115,8 @@ const styles = StyleSheet.create({
 		fontWeight: "800",
 		fontSize: 24,
 		textAlign: "left",
-		paddingBottom: 16,
+		paddingBottom: 12,
+		paddingLeft: 10,
 	},
 	textDescription: {
 		color: "#f5f5f5",
@@ -113,7 +128,7 @@ const styles = StyleSheet.create({
 	button: {
 		margin: 24,
 		borderRadius: 12,
-		backgroundColor: "black",
+		backgroundColor: "#0f0f0f",
 		width: 200,
 		alignSelf: "center",
 	},
