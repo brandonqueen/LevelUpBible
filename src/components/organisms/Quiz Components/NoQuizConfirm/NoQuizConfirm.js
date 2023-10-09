9b5125c6ad419089a49ad349fd2f1fbf3085a13d
comboy3004 <brandonqueen87@gmail.com>
2023-10-09 11:11:00 -0400

    NoQuizConfirm cleanup

--- src/components/organisms/Quiz Components/NoQuizConfirm/NoQuizConfirm.js
+++ src/components/organisms/Quiz Components/NoQuizConfirm/NoQuizConfirm.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, ScrollView } from "react-native";
 import StyledTextButton from "../../../atoms/StyledTextButton/StyledTextButton";
+import colors from "../../../../constants/colors";
 
 const NoQuizConfirm = ({
 	bookName,
@@ -7,12 +8,13 @@ const NoQuizConfirm = ({
 	modalToggle,
 	handleConfirmPressed,
 }) => {
+	//BOOKS THAT HAVE QUIZ DATA
 	const lastBook = currentQuizBooks.slice(-1);
 	const otherBooks = currentQuizBooks.slice(0, -1);
 
 	return (
 		<ScrollView showsVerticalScrollIndicator={false}>
-			<Text style={styles.header}>Quiz Coming Soon 🧐</Text>
+			<Text style={styles.header}>Quiz Coming Soon{"\n"}🧐</Text>
 			<Text style={styles.textDescription}>
 				We currently do not have a quiz for {bookName}. We currently have
 				quizzes for the following books of the Bible:{"\n"}
@@ -32,15 +34,15 @@ const NoQuizConfirm = ({
 				I confirm that I have read this chapter
 			</Text>
 			<StyledTextButton
-				backgroundColor={"#695DDA"}
-				backgroundPressedColor={"#8073ff"}
+				backgroundColor={colors.secondaryLight}
+				backgroundPressedColor={colors.secondaryLighter}
 				margin={20}
 				onPress={handleConfirmPressed}>
 				Confirm
 			</StyledTextButton>
 			<StyledTextButton
-				backgroundColor={"#cf4b38"}
-				backgroundPressedColor={"#e85b46"}
+				backgroundColor={colors.quarternary}
+				backgroundPressedColor={colors.quarternaryLight}
 				margin={20}
 				onPress={modalToggle}>
 				Cancel
@@ -53,34 +55,37 @@ export default NoQuizConfirm;
 
 const styles = StyleSheet.create({
 	header: {
-		fontSize: 25,
+		fontSize: 30,
 		fontWeight: "900",
 		textAlign: "center",
-		color: "#f5f5f5",
-		margin: 24,
+		color: colors.text,
+		marginVertical: 24,
 		lineHeight: 40,
 	},
 	textDescription: {
-		color: "#f5f5f5",
+		color: colors.text,
 		fontWeight: "400",
 		fontSize: 18,
 		textAlign: "left",
 		paddingHorizontal: 12,
+		letterSpacing: 0.2,
 	},
 	currentBooks: {
-		color: "#f5f5f5",
-		fontWeight: "400",
-		fontSize: 18,
+		color: colors.text,
+		fontWeight: "700",
+		fontSize: 19,
 		textAlign: "left",
 		paddingHorizontal: 12,
-		paddingLeft: 24,
+		paddingLeft: 28,
+		letterSpacing: 0.3,
 	},
 	confirmation: {
-		color: "rgb(250, 250, 125)",
+		color: colors.tertiary,
 		fontWeight: "800",
 		fontSize: 20,
 		textAlign: "center",
 		paddingVertical: 10,
 		textTransform: "uppercase",
+		letterSpacing: 0.3,
 	},
 });
